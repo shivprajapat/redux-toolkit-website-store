39288e77d961713c1b951cf029fb92217c2fc2a2
yudizshiv <104143801+yudizshiv@users.noreply.github.com>
2022-06-09 17:44:31 +0530

    createAsyncThunk api call

--- src/store/features/productSlice.jsx
+++ src/store/features/productSlice.jsx
@@ -1,4 +1,4 @@
-import { createSlice } from "@reduxjs/toolkit";
+import { createSlice, createAsyncThunk } from "@reduxjs/toolkit";
 
 export const STATUSES = Object.freeze({
     IDLE: 'idle',
@@ -13,30 +13,28 @@ const productSlice = createSlice({
         data: [],
         status: STATUSES.IDLE
     },
-    reducers: {
-        setProducts(state, action) {
-            state.data = action.payload;
-        },
-        setStatus(state, action) {
-            state.status = action.payload;
-        }
-    },
+    extraReducers: (builder) => {
+        builder
+            .addCase(fetchProducts.pending, (state) => {
+                state.status = STATUSES.LOADING
+            })
+            .addCase(fetchProducts.fulfilled, (state, action) => {
+                state.data = action.payload;
+                state.status = STATUSES.IDLE
+            })
+            .addCase(fetchProducts.rejected, (state) => {
+                state.status = STATUSES.ERROR
+            })
+    }
 })
 
 export const { setProducts, setStatus } = productSlice.actions;
 export default productSlice.reducer;
 
-export function fetchProducts() {
-    return async function fetchProductThunk(dispatch, getState) {
-        dispatch(setStatus(STATUSES.LOADING));
-        try {
-            const res = await fetch('https://fakestoreapi.com/products');
-            const data = await res.json();
-            dispatch(setProducts(data));
-            dispatch(setStatus(STATUSES.IDLE));
-        } catch (err) {
-            console.log(err);
-            dispatch(setStatus(STATUSES.ERROR));
-        }
-    };
-}
+// Thunk
+
+export const fetchProducts = createAsyncThunk('products/fetch', async () => {
+    const res = await fetch('https://fakestoreapi.com/products');
+    const data = await res.json();
+    return data
+})
